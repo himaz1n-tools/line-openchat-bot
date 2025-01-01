09fe5cde174294175f1dc10d3d524dc46a60c406
himaz1n-tools <kyoeltyan0204@gmail.com>
2025-01-01 17:17:06 +0900

    Create config.ts

---
+++ config.ts
@@ -0,0 +1,5 @@
+export const config = {
+  channelAccessToken: "YOUR_CHANNEL_ACCESS_TOKEN", // LINEチャネルのアクセストークン
+  channelSecret: "YOUR_CHANNEL_SECRET",           // LINEチャネルのシークレット
+  groupId: "YOUR_GROUP_ID",                       // 対象グループID
+};
